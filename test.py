ea29acd405e4400e1ac914f96398abaf576f944d
beijixiaohu <yuhao888123@gmail.com>
2023-03-21 21:58:36 +0800

    rep

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from KeyboardDateComm import KeyboardDateComm
 from MouseDateComm import MouseDateComm
 
 serial.ser = serial.Serial('COM4', 9600)  # 开启串口
+
 # 键盘输出helloworld
-# dc = KeyboardDateComm()
-# dc.send_data('HHEELLLLOO')  # 按下HELLO
-# dc.release()  # 松开
-# dc.send_data('WWOORRLLDD')  # 按下WORLD
-# dc.release()  # 松开
+dc = KeyboardDateComm()
+dc.send_data('HHEELLLLOO')  # 按下HELLO
+dc.release()  # 松开
+dc.send_data('WWOORRLLDD')  # 按下WORLD
+dc.release()  # 松开
 
 
 # （绝对）鼠标移动到屏幕的左上100*100的位置
