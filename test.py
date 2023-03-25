979797066bc71fc12666f097c41596922973ec79
beijixiaohu <yuhao888123@gmail.com>
2023-03-25 16:05:12 +0800

    Update

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from ch9329Comm import mouse
 serial.ser = serial.Serial('COM4', 115200)  # 开启串口
 
 # # 键盘输出helloworld
-# dc = KeyboardDataComm()
+# dc = keyboard.DataComm()
 # dc.send_data('HHEELLLLOO')  # 按下HELLO
 # dc.release()  # 松开
 # dc.send_data('WWOORRLLDD')  # 按下WORLD
@@ -13,17 +13,16 @@ serial.ser = serial.Serial('COM4', 115200)  # 开启串口
 
 
 # （绝对）鼠标移动到屏幕的左上100*100的位置
-# from MouseDataComm import MouseDataComm
-#
-# dc = MouseDataComm()
+
+# dc = mouse.DataComm()
 # dc.send_data_absolute(500,500)
 
 # （相对）鼠标右移100px 下移100px
-# dc2 = MouseDataComm()
+# dc2 = mouse.DataComm()
 # dc2.move_to(-100, -100)
 
 # # 校验
-# dc2 = MouseDataComm()
+# dc2 = mouse.DataComm()
 # dc2.move_to(-230,-480)
 
 serial.ser.close()  # 关闭串口
